fc07cd9d0328221b383c705125870b71e76ca143
assia <o@gmail.com>
2025-01-12 00:23:35 +0000

    v2.3

--- src/main/java/com/workify/workify_ag/Controllers/adminControllers/CategorieController.java
+++ src/main/java/com/workify/workify_ag/Controllers/adminControllers/CategorieController.java
@@ -2,7 +2,7 @@ package com.workify.workify_ag.Controllers.adminControllers;
 
 import com.workify.workify_ag.Entities.Categorie;
 import com.workify.workify_ag.Services.CategorieService.CategorieService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,38 +11,39 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class CategorieController {
-    @Autowired
-    private CategorieService categorieService;
+
+    private final CategorieService categorieService;
 
     // Get all categories
-    @GetMapping("/admin/categories")
+    @GetMapping("/allCategorie")
     public List<Categorie> getAllCategories() {
         return categorieService.getAllCategories();
     }
 
     // Get category by ID
-    @GetMapping("/categories/{id}")
+    @GetMapping("/categorie/{id}")
     public ResponseEntity<Categorie> getCategorieById(@PathVariable Long id) {
         Optional<Categorie> categorie = categorieService.getCategorieById(id);
         return categorie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     // Get category by libelle
-    @GetMapping("/categories/libelle/{libelle}")
+    @GetMapping("/categorie/libelle/{libelle}")
     public ResponseEntity<Categorie> getCategorieByLibelle(@PathVariable String libelle) {
         Optional<Categorie> categorie = categorieService.getCategorieByLibelle(libelle);
         return categorie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     // Save a new category
-    @PostMapping("/categories/ajouterCategorie")
+    @PostMapping("/categorie/add")
     public Categorie saveCategorie(@RequestBody Categorie categorie) {
         return categorieService.saveCategorie(categorie);
     }
 
     // Delete a category by ID
-    @DeleteMapping("/supcategorie/{id}")
+    @DeleteMapping("/categorie/delete/{id}")
     public ResponseEntity<Void> deleteCategorie(@PathVariable Long id) {
         categorieService.deleteCategorie(id);
         return ResponseEntity.noContent().build();
